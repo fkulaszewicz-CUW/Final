53edb2a57457f1c951c558f97380500131043ead
Frank Kulaszewicz <fkulaszewicz@wisc.edu>
2021-06-16 21:40:24 -0500

    Website Created

--- car-stats/src/app/app-routing.module.ts
+++ car-stats/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarProfileComponent } from './pages/car-profile/car-profile.component';
+import { WelcomeScreenComponent } from './pages/welcome-screen/welcome-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WelcomeScreenComponent },
+  { path: 'car-profile', component: CarProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- car-stats/src/app/app.module.ts
+++ car-stats/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeScreenComponent } from './pages/welcome-screen/welcome-screen.component';
+import { CarProfileComponent } from './pages/car-profile/car-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeScreenComponent,
+    CarProfileComponent
   ],
   imports: [
     BrowserModule,
